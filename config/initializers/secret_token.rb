2a76c93646697dec41eb0776295053326a81172e
Ryland Herrick <ryland@bendyworks.com>
2012-03-30 15:53:56 -0500

    First commit.  Stock rails 3.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EyeRed::Application.config.secret_token = 'b2b4902fd7405419d8e242e5e202f6109e6bd6e37c4aa77869aeaa76ca28e3c7cc46c7c8c50a508e19a01fb01a23ba5ab4e941b634e94b01620c10f9b81206d5'
